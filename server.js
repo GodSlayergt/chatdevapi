f50510e90f2e638d2f3f7c2300977cd6421c7733
Godslayergt <gtiwari037@gmail.com>
2023-08-06 14:38:38 +0530

    fix: minor changes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors")
 const { chats } = require("./data/data");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
@@ -12,27 +13,12 @@ const path = require("path");
 dotenv.config();
 connectDB();
 const app = express();
+app.use(cors({origin:process.env.CLIENT_URI,optionsSuccessStatus:200}))
 app.use(express.json());
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
-// --------------------------deployment------------------------------
-
-const __dirname1 = path.resolve();
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
-
-// --------------------------deployment------------------------------
 
 app.use(notFound);
 app.use(errorHandler);
@@ -45,7 +31,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URI,
   },
 });
 io.on("connection", (socket) => {
